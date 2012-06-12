628c961ab8aa7213a2c3682134552c23428aa2f9
Tim Ray <tr@getupstream.com>
2012-06-12 15:42:59 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeployTest::Application.config.secret_token = '8223ce91d57d8a8c7a6da634f1f39c450056a5d303270350c0bef5f60ed35b250075df097d53417ee6fd2a4103c635ec502f73565d921714c0095354f33be50a'
