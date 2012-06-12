69f724028d4bb5a79920c5f3a191764732eb2f1a
Tim Ray <tr@getupstream.com>
2012-06-12 16:33:23 -0500

    update for weird typo

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-DeployTest::Application.config.session_store :cookie_store, key: '_deploy_test_session'
+DeployTest::Application.config.session_store :cookie_store, :key => '_deploy_test_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
