8838f8f831b2af1a0571295c97d2fe5c97211547
Tim Ray <tr@getupstream.com>
2012-06-12 16:08:44 -0500

    updates for deploy test

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,41 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+require 'bundler/capistrano'
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "deploy_test"
+set :repository,  "git@github.com:dr-stangecode/deploy_test.git"
+set :deploy_to, "/opt/numerex/rails/"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
+set :scn_verbose, true
+set :keep_releases, "4"
+set :use_sudo, false
+set :deploy_via, :copy
+set :copy_cache, false
+set :copy_exclude, ['.git']
+set :user, 'numerex'
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+
+  desc "Create symlinks for shared image upload directories"
+  task :create_symlinks do
+    run "ln -fs #{shared_path}/database.yml #{release_path}/config/database.yml"
+  end
+
+  after 'deploy:update_code', 'deploy:create_symlinks'
+end
+
+task :staging do
+  set :rails_env, 'loadtest'
+  set :branch, 'staging'
+  role :web, "puppet-test.mrpbx.com"                          # Your HTTP server, Apache/etc
+  role :app, "puppet-test.mrpbx.com"                          # This may be the same as your `Web` server
+  role :db,  "puppet-test.mrpbx.com", :primary => true # This is where Rails migrations will run
+end
+
 
 # if you want to clean up old releases on each deploy uncomment this:
 # after "deploy:restart", "deploy:cleanup"
